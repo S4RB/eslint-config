f2740f0c9a6b254f425fab2938f86cb6f7f7702d
Rob Scott <rob.scott@s4rb.com>
2017-10-19 08:51:09 +0100

    Use general purpose no-uppercase-require rule

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ module.exports = {
 		'@s4rb/s4rb-custom/no-mixed-require': 2,
 		'@s4rb/s4rb-custom/no-multiline-bluebird-join': 2,
 		'@s4rb/s4rb-custom/no-relative-module-path': 2,
-		'@s4rb/s4rb-custom/no-uppercase-bluebird-require': 2,
+		'@s4rb/s4rb-custom/no-uppercase-require': 2,
 		'@s4rb/s4rb-custom/no-translate-instant': 1,
 		'semi': [2, 'always'],
 		'sort-imports': 2,
